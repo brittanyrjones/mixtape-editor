a64068650a8b7fa98cee06877cf8b7badc970854
Brittany <brittany.r.jones@hotmail.com>
2020-08-25 13:29:28 -0700

    adding readme updates, editing for missing args

--- README.md
+++ README.md
@@ -1,28 +1,66 @@
-# mixtape-editor
-ingest input, ingest changes to that input, produce output, using batches
+# Acceptance
 
-For this exercise, you will write 3 functions for a command-line batch application. Highspot's product leverages batch processing so this exercise is relevant to real-life Highspot engineering work.This should take between one and two hours to complete.
-
-IMPLEMENTATION SUMMARY
-Your application will
-- Ingest an input JSON file which we will provide, mixtape.json.
-- Ingest a changes file which you will create.
-- Produce output.json which must have the same structure as the mixtape.json input.
-- You choose the format for the changes file -text, YAML, CSV, JSON, etc.  
-- Your application will apply the changes from the change file to the mixtape.json data, and produce output.json.
+Ingest mixtape.json and a changesfile. The changesfile should include multiple changes in a single file: Add a new playlist; the playlist should contain at least one song. Remove a playlist. Add an existing song to an existing playlist. Output output.json using the same structure as mixtape.json with the changes applied. Add a README describing how you would scale this application to handle very large input files and/or very large changes files.
 
 
-REQUIREMENTS
-Ingest mixtape.json and a changes file.  
-The changesfile should include multiple changes in a single file:
-* Add a new playlist; the playlist should contain at least one song.
-* Remove a playlist.
-* Add an existing song to an existing playlist.
-Output output.json using the same structure as mixtape.json with the changes applied.
-Add a README describing how you would scale this application to handle very large input files and/or very large changes files.
+## Installation
 
-EVALUATION CRITERIA
-Are all requirements met?
-Does the application run successfully on a Mac or Linux?
-Does your README explain how to run your application?
-Have you provided justification(s) in your scaling discussion?
+Clone this project:
+```
+$ git clone https://github.com/brittanyrjones/mixtape-editor.git
+```
+
+ # Running the Script
+
+Make sure you are in the right directory:
+```
+$ cd mixtape-editor
+```
+
+Now you can run the script:
+```
+$ python3 mixtape_editor.py mixtape.json changes
+```
+ 
+ 
+ # Scaling Up
+ 
+ My code would not run optimally given a larger data set, with larger files. 
+ In my current working example, the files being injested are being loaded into memory from
+ JSON files, and then I am running CRUD on selected resources. 
+
+I would consider making the following changes to scale up:
+
+First of all, I would start by writing tests. It would be important to validate all objects before starting to manipulate them. 
+
+### The Changes file ingestion 
+ * Using a stream/batch based approach in reading file. 
+ - Reading the changes file and writing separate files (into batches) based on intended CRUD operation. Each "CRUD" file would contain all of the related changes. If we batched the processes by action (add, delete, update), it would be easier to keep track of what has been processed in the queue.
+    * One downside to this idea: What if there are 1 million delete actions, and nothing else? In this case, we should consider batching the file by line....
+ - This would help with reducing memory in space needed for reading changes file. 
+ - This would also be helpful for keeping records in sync, as we could lock our resources as we make changes to them.
+ 
+ ## The Mixtape file ingestion
+ * Using a database (mongodb, for example) to load data.
+ * Using a stream/batch based approach in reading file. 
+ - Reading the changes file and writing separate files (into batches) based on intended CRUD operation. Each "CRUD" file would contain all of the related changes. If we batched the processes by action (add, delete, update), it would be easier to keep track of what has been processed in the queue.
+    * One downside to this idea: What if there are 1 million playlist actions, and nothing else? In this case, we should consider batching the file by line....
+ - By loading the data into a database, it would be easier to separate smaller chunks of data to then read through.
+ - It may also be easier to use a json serializing/deserializing library to read each mixtape object line by line into a Database or a new batch file, reducing the chances of running out of memory.
+
+ ## Using multiple process 
+ * Using multiple processes to read the input file into a database or batch files (the batch files then can be used in smalller chunks if a database is not an option), and also using multiple processes to write
+ the changes data to the database, or batch files. 
+ 
+ ## Refactoring the class
+ I would also refactor the class to create playlist data type objects based on the file data. This would help
+ with type checking the file data, and cleaning the data before it gets pushed into a database. I would also
+ write a lot of different helper methods, it would be helpful for each function to have only one job, for example, 
+ just adding an item, or deleting an item, instead of doing a "changes" function that does both. 
+ 
+ # In Closing
+Thank you for taking the time to check out my code! There were so many ways to do this depending on
+so many factors (memory, space, budget, throughput, etc), and I appreciate you
+taking the time to look through my coding lense. I look forward to hearing from you and the team 
+ 
+*Brittany Jones
--- mixtape_editor.py
+++ mixtape_editor.py
@@ -1,124 +1,111 @@
+import argparse
 import json
-import sys
 import os
-import multiprocessing as mp
 
-# import argparse
-
-# If scaling up, I would use a class with more helper methods
-# VS having 3 functions doing the work.
-# In the class (or classes), it would be easy to define specific attributes
-# for each object. I was thinking, it would be important to
-# normalize the mixtape data, maybe by creating a class object of mixtape
-# and then initializing that class with all of the attributes that the
-# mixtape needs to be considered valid. That way, it would be less complex
-# and more DRY to type check when adding and deleting mixtape items.
+import sys
 
 
-def delete_mixtape_object(mixtape_data: dict, change_object: dict):
-    '''
-    This function consumes mixtape data object, and dumps
-    it into an output file.
+class Mixtape_Editor:
+    # When refactoring for optimal code, I would initialize my class with the changes and mixtape data, and objects needed
+    # for logging. I would also use way more helper functions, for typechecking and to simplify
+    # adding, deleting, and updating items.
+    # I would also be more specific about the structure of each input object for the class functions
 
-    params - dict: mixtape_data
-    '''
-    # Delete mixtape object
-    for key, value in mixtape_data.items():
-        for item in value:
-            # making sure they are the same key, and same id.
-            if change_object['id'] == item['id'] and change_object[
-                    'type'] == key:
-                value.remove(item)
+    # ex.
 
-def add_mixtape_object(mixtape: dict, change_object: dict):
-    '''
-    This function will first check to change object action. If the action
-    is add, we look through each key value pair and assert that if the change object is
-    playlist and the key to the mixtape objects we are looping though is also playlist,
-    then we can assume we are adding the playlist(change object) into the correct key value.
-    We then assert that if the change object type is song, a song is being added and also 
-    if the key is playlist, then a song is being added to a playlist. 
+    # def __init__(self, mixtape_data, changes_data):
+        # self.logger = logger
+        # self.mixtape_data = mixtape_data
+        # self.changes_data = changes_data
 
-    params - dict: change_object
-             dict: mixtape
-    '''
+    def run_mixtape_object_edits(self, mixtape_data: dict, change_object: dict):
+        '''
+        This function consumes mixtape data object, changes object. 
+        The data is parsed from the changes, and depending on the action being taken (add or delete),
+        the function will determine which item should be acted upon, and from which object (users, playlists, songs)
+        it should be acted upon in the within the mixtape.
 
-    # before adding and item, it would be good to make sure the item_id
-    # doesnt already exist within the dataset.
-    # in the real world data with large data sets that
-    # arent created by me, there can be no
-    # certainty. It would help to assure the data is clean either
-    # by using a certain database, or manually clean the data,
-    # by writing some lines that will randomize and check for dup item id numbers.
+        params - dict: mixtape_data
+                 dict: change_object
+        '''
 
-    # Add a playlist. 
-    for key, value in mixtape_data.items():
-        if change_object['type'] == 'playlists' and key == 'playlists':
-
-            value.append({
-                "id": change_object['id'],
-                "user_id": change_object['user_id'],
-                "song_ids": change_object['song_ids']
-            })
-
-        # Adds a song to an existing playlist
-        if change_object['type'] == 'songs' and key == 'playlists':
-            for item in value:
-                if change_object['playlist_id'] == item['id']:
-                    item['song_ids'].append(change_object['id'])
-
-#TODO: MUTLIPROCESS THE DATA IN MEMORY
-def get_data(input_file :str):
-    '''
-    This function takes in data from a file, and returns that data as a dict.
-
-    params - str: input file
-    '''
-    
-
-    data = {}
-
-    if not os.path.exists(input_file):
-        sys.exit(f'ERROR: unable to locate file {input_file}')
-
-    with open(input_file) as infile:
-        input_object = json.load(infile)
-
-    data.update(input_object)
-
-    return data
-
-
-# Since we know exactly what we want to injest, im going to keep it simple
-# by just checking that all 3 arguments are there, and printing out
-# a basic usage if there is an argument(input file) missing.
-if sys.argv[1] != "mixtape.json" or len(sys.argv) < 3:
-    sys.exit(f'Usage: {sys.argv[0]} mixtape.json changes')
-
-mixtape_data = get_data(sys.argv[1])
-changes_data = get_data(sys.argv[2])
-
-for key, value in changes_data.items():
-    for change_item in value:
-        if change_item['action'] == 'add':
-            add_mixtape_object(mixtape_data, change_item)
-        if change_item['action'] == 'delete':
-            delete_mixtape_object(mixtape_data, change_item)
+        for key, value in mixtape_data.items():
+            # Delete mixtape object
+            if change_object['action'] == "delete":
+                for item in value:
+                    # making sure they are the same key, and same id.
+                    if change_object['id'] == item['id'] and change_object[
+                            'type'] == key:
+                        value.remove(item)
             
-#TODO: MUTLIPROCESS THE DATA writing
+            # Add mixtape object
+            elif change_object['action'] == "add":
+                if change_object['type'] == 'playlists' and key == 'playlists':
+                    value.append({
+                        "id": change_object['id'],
+                        "user_id": change_object['user_id'],
+                        "song_ids": change_object['song_ids']
+                    })
+                # Adds a song to an existing playlist
+                if change_object['type'] == 'songs' and key == 'playlists':
+                    for item in value:
+                        if change_object['playlist_id'] == item['id']:
+                            item['song_ids'].append(change_object['id'])
 
-with open('output.json', 'w') as outfile:
-    json.dump(mixtape_data, outfile, indent=2, separators=(',', ' : '))
 
-# execute_changes("changes.json")
+    def get_data(self, input_file :str):
+        '''
+        This function takes in a file, and returns the data as a dict.
 
-# parser = argparse.ArgumentParser()
+        params - str: input file
+        '''
+        
+        data = {}
+        if not os.path.exists(input_file):
+            sys.exit(f'ERROR: unable to locate file {input_file}')
 
-# parser.add_argument("-c", "--change", help="Password")
-# parser.add_argument("-m", "--mixtape", help="Size")
+        with open(input_file) as infile:
+            input_object = json.load(infile)
 
-# args = parser.parse_args()
+        data.update(input_object)
 
-# mixtape_data = get_data(args.mixtape)
+        return data
+        
+    def output_data(self, data, output_file):
+        '''
+        This function takes in data, and an output file. The data is dumped 
+        to the output file, output.json
 
-# print(mixtape_data)
+        params - dict: data 
+                 str: output file
+        '''
+
+        with open(output_file, 'w') as outfile:
+            # Keeping the format EXACTLY the same
+            json.dump(data, outfile, indent=2, separators=(',', ' : '))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=
+        '')
+    parser.add_argument('mixtape', required=True)
+    parser.add_argument('changes', required=True)
+
+    if len(sys.argv) < 2:
+        sys.stderr.write('Usage: ')
+        sys.exit(1)
+    else:
+        args = parser.parse_args()
+
+        mixtape_editor = Mixtape_Editor()
+
+        mixtape_data = mixtape_editor.get_data(args.mixtape)
+        changes_data = mixtape_editor.get_data(args.changes)
+
+        for key, value in changes_data.items():
+            for change_item in value:
+                mixtape_editor.run_mixtape_object_edits(mixtape_data, change_item)
+
+        mixtape_editor.output_data(mixtape_data, 'output.json')   
+    
